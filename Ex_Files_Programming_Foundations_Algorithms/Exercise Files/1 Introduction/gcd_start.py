9e2173c9b44de62e3a2434fbcf42506f6949619f
GeroZayas <gerozayas@gmail.com>
2022-11-25 11:57:34 +0100

    add efficient solution

--- Ex_Files_Programming_Foundations_Algorithms/Exercise Files/1 Introduction/gcd_start.py
+++ Ex_Files_Programming_Foundations_Algorithms/Exercise Files/1 Introduction/gcd_start.py
@@ -5,23 +5,34 @@ import time
 start_time = time.time()
 
 
-def gcd(x, y) -> int:
-    z: int
-    found: bool = False
-    top_num: int = min(x, y)
-    # print("this is top num", top_num)
-    z = top_num
-    if x % y == 0 or y % x == 0:
-        return top_num
-    while not found:
-        z -= 1
-        if x % z == 0 and y % z == 0:
-            found = True
-            return z
+# def gcd(x, y) -> int:
+#     z: int
+#     rest = max(x, y) // min(x, y)
+#     found: bool = False
+#     top_num: int = min(x, y)
+#     # print("this is top num", top_num)
+#     z = top_num
+#     if x % y == 0 or y % x == 0:
+#         return top_num
+#     while not found:
+#         z -= rest
+#         if x % z == 0 and y % z == 0:
+#             found = True
+#             return z
+
+
+# more efficient function:
+def gcd(x, y):
+    while y != 0:
+        t = x
+        x = y
+        y = t % y
+
+    return x
 
 
 # try out the function with a few examples
-print(gcd(57596581, 49039424))  # should be 959
+print(gcd(31812426, 29804539))  # should be 359
 
 final_time = time.time() - start_time
 
