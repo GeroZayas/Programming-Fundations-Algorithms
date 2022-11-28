906ba9e86c580f2e891105cb1b689cef67357df5
GeroZayas <gerozayas@gmail.com>
2022-11-28 22:54:26 +0100

    Update

--- Ex_Files_Programming_Foundations_Algorithms/Exercise Files/2 Data Structures/Stacks/stack_start.py
+++ Ex_Files_Programming_Foundations_Algorithms/Exercise Files/2 Data Structures/Stacks/stack_start.py
@@ -1,9 +1,13 @@
 # try out the Python stack functions
 
 # TODO: create a new empty stack
-
+stack = []
 
 # TODO: push items onto the stack
+stack.append(1)
+stack.append(2)
+stack.append(3)
+stack.append(4)
 
 
 # TODO: print the stack contents
